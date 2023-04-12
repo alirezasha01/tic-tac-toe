7296411dc6d0c65225f61b2b55309a6ddb34489d
Alireza <alirezasha01@gmail.com>
2023-04-12 22:39:48 +0430

    One is placed between X and O

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import "./style.css"
 import Square from "./components/Square";
 
 function App() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
