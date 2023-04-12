17df09a31d4d13466417419729879003e87632e2
Alireza <alirezasha01@gmail.com>
2023-04-12 22:04:40 +0430

    Static to dynamic conversion table and change table data to X

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import "../style.css"
 
-const Square = ({value}) => {
-    return(
-    <>
-      <button className="square">{value}</button>
-    </>
-    )
+const Square = () => {
+const [value, setValue] = useState(null);
+function handleClick() {
+    setValue('X');
+}
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default Square
